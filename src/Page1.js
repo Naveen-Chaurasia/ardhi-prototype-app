f1b785ab1a4d98ac6109907de94a39a7e57dc5cb
Naveen <naveen.chaurasia861@gmail.com>
2022-06-14 09:46:16 +0530

    updated page1 and page2(embedded <iframe/>)

--- src/Page1.js
+++ src/Page1.js
@@ -8,17 +8,35 @@ import "./style.css";
 
 function Page1() {
     const [isClicked, setIsClicked] = useState(false);
+    
   // React States
   const navigate= useNavigate();
 
 //   function getElementByXpath(path) {
 //     return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 //   }
-  const imageClick = () => 
+  const imageClick1 = () => 
+ {
+      navigate('/Page2')
+//     setIsClicked(true);
+//     console.log("^^^^^^^^^^^^^^^^^");
+//    window.location.href = "http://localhost:3000/";
+//   //  window.location.href = "https://bloom.neo4j.io/index.html?connectURL=neo4j%2Bs%3A%2F%2F6799f560.databases.neo4j.io&_ga=2.105062592.230755171.1655092750-1401971628.1655092750";
+//     console.log("^^^^^^^^^^^^^^^^^");
+//   let x=  document.getElementsByClassName("MuiButton-label")[1].click();
+  
+//     //document.getElementsByClassName("MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedSizeLarge MuiButton-sizeLarge").click();
+// console.log("*********************");
+//     //    let x= getElementByXpath("/html/body/div[2]/div[3]/div/div[2]/button[2]")    ;
+    
+ }
+
+ const imageClick = () => 
  {
     setIsClicked(true);
     console.log("^^^^^^^^^^^^^^^^^");
     window.location.href = "http://localhost:3000/";
+  // window.location.href = "https://bloom.neo4j.io/index.html?connectURL=neo4j%2Bs%3A%2F%2F6799f560.databases.neo4j.io&_ga=2.105062592.230755171.1655092750-1401971628.1655092750";
     console.log("^^^^^^^^^^^^^^^^^");
   let x=  document.getElementsByClassName("MuiButton-label")[1].click();
   
@@ -36,10 +54,60 @@ console.log("*********************");
   return (
     <div className="app">
         <img src="logo.png" height="100px" width="100px" />
-    
-    <div className="page1_image" id="pi">                                                                        
-      <img  src="cafe1.png" height="500px" width="900px"  padding="10p0x" onClick={() => imageClick()}/>
+       
+    <div className="page1_image" id="pi"> 
+                            
+      <img  src="cafe1.png" height="500px" width="900px"  padding="10px"  onClick={() => imageClick()}/>
+      <table className="Table1" >
+      <caption >GRPF Values</caption>
+        <tr>
+          <th>Material</th>
+          <th>Property</th>
+          <th>Value</th>
+        </tr>
+        <tr>
+          <td>GFRP</td>
+          <td>Energy</td>
+          <td>5.14</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>Toxicity</td>
+          <td>0.59ppmv</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>CarbonEmission</td>
+          <td>8.3ppmv</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>Water</td>
+          <td>80Kg</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>ReuseByRecycle%</td>
+          <td>30%</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>Location</td>
+          <td>Manama</td>
+        </tr>
+        {/* <tr>
+          <td onClick={() => imageClick1()}><button class="button button1">View Graph</button></td>
+          <td></td>
+          <td onClick={() => imageClick()}><button class="button button1">View Chart</button></td>
+        </tr> */}
+      </table>  
+      <div4 className="buttn">
+      <button class="button button1" onClick={() => imageClick1()}>View Graph</button>  
+      <button class="button button1" onClick={() => imageClick()}>View Chart</button>
+      <div4/>
+      </div4>
     </div>
+    
     {/* {isClicked? navigate('/Page2'):renderForm} */}
     {/* <div>  <input type="submit"  name="view Dashboard"/></div> */}
     <Footer/>
--- src/Page2.js
+++ src/Page2.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import {Link, Route, Router, useNavigate} from 'react-router-dom';
 import "./style.css";
 
-
+import Iframe from 'react-iframe'
+import Footer from "./Footer";
 
 function Page2() {
     const [isClicked, setIsClicked] = useState(false);
@@ -21,16 +22,34 @@ function Page2() {
   );
   
   return (
-    <div className="page2">
-    {/* <t>TITLE2</t>   */}
-    <div className="page1_image2" id="pi2">                                                                        
-      <img src="dashboard.png" height="700px" width="1500px"  onClick={() => imageClick()}/>
-      {/* <Link to={{ pathname: "http://neodash.graphapp.io/" }} target="_blank" /> */}
-    </div>
-    {/* {isClicked? navigate('http://neodash.graphapp.io/'):renderForm}
-    <t1>  <input type="submit"  name="view Dashboard"/></t1> */}
-    </div>
-   
+    // <div className="page2">
+    // {/* <t>TITLE2</t>   */}
+    // <div className="page1_image2" id="pi2">                                                                        
+    //   <img src="dashboard.png" height="700px" width="1500px"  onClick={() => imageClick()}/>
+    //   {/* <Link to={{ pathname: "http://neodash.graphapp.io/" }} target="_blank" /> */}
+    // </div>
+    // {/* {isClicked? navigate('http://neodash.graphapp.io/'):renderForm}
+    // <t1>  <input type="submit"  name="view Dashboard"/></t1> */}
+    // </div>
+    <div className="app">
+      <img src="logo.png" height="70px" width="70px" />
+      <div className="page1_image">
+    <div className="iframe">
+    <Iframe
+     url="https://bloom.neo4j.io/index.html?connectURL=neo4j%2Bs%3A%2F%2F6799f560.databases.neo4j.io&_ga=2.172551520.230755171.1655092750-1401971628.1655092750"
+   // url="https://browser.neo4j.io/?connectURL=neo4j%2Bs%3A%2F%2Fneo4j%406799f560.databases.neo4j.io%2F&_ga=2.105062592.230755171.1655092750-1401971628.1655092750"
+    
+    width="1300px"
+            height="600px"
+            id="myId"
+            className="myClassname"
+            display="initial"
+            position="relative"/>
+
+   </div>
+   <Footer/>
+   </div>
+   </div>
   );
 }
 
