231b4fadfe950764d93f7d857e1463930b76de17
Naveen <naveen.chaurasia861@gmail.com>
2022-07-12 14:51:28 +0530

    changed url for knowledge graph

--- src/Page1.js
+++ src/Page1.js
@@ -16,7 +16,8 @@ function Page1() {
 //   }
   const imageClick1 = () => 
  {
-      navigate('/Page2')
+  window.location.href = "http://localhost:8110/visualization";
+     // navigate('/Page2')
 //     setIsClicked(true);
 //     console.log("^^^^^^^^^^^^^^^^^");
 //    window.location.href = "http://localhost:3000/";
@@ -35,7 +36,8 @@ function Page1() {
     setIsClicked(true);
     console.log("^^^^^^^^^^^^^^^^^");
     //window.location.href ="https://naveen.budibase.app/app_dev_naveen_77d34a3d02ab4aa2889411486d714cee#/home";
-   window.location.href = "http://localhost:3000/";
+  // window.location.href = "http://localhost:3000/";
+  window.location.href = "https://naveen.budibase.app/app/ardhi_lca#/loginpage";
   //  window.location.href = "https://bloom.neo4j.io/index.html?connectURL=neo4j%2Bs%3A%2F%2F6799f560.databases.neo4j.io&_ga=2.105062592.230755171.1655092750-1401971628.1655092750";
     console.log("^^^^^^^^^^^^^^^^^");
   let x=  document.getElementsByClassName("MuiButton-label")[1].click();
